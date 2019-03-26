f3bf4be7adc94139f7e939b4361db22d28726348
KhanhIceTea <khanhicetea@gmail.com>
2019-03-26 18:39:19 +0700

    debug

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -33,6 +33,7 @@ function soundalert()
                 var project_id = Object.byString(notification, 'event_data.task.project_id') || 0;
                 var task_id = Object.byString(notification, 'event_data.task.id') || 0;
                 var task_link = (project_id && task_id) ? "/?controller=TaskViewController&action=show&project_id="+project_id+"&task_id="+task_id : '/';
+                console.log(task_link);
 
                 Push.create(push_title, {
                     body: notification.title,
